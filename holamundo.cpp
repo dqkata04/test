970d7718358c4f6e0ef678216c82ee8be8a71db6
dqkata04 <107488500+dqkata04@users.noreply.github.com>
2022-06-14 09:31:06 -0500

    Add files via upload

---
+++ holamundo.cpp
@@ -0,0 +1,14 @@
+// Hello world example in C++
+#include <iostream>
+using namespace std;
+int main()
+{
+for (int i=1; i<=10; i++) {
+cout << i << " Hello World, Folks" << endl ;
+if ( i == 7) {
+cout << " that was lucky!\n" << endl ;
+} else {
+cout << endl ;
+}
+}
+}
